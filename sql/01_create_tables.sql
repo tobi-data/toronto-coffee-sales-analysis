aba55d19108882c87df21c813c741f4265192f0e
tobi-data <tobiawolaja987@gmail.com>
2025-12-26 09:46:14 +0100

    Add table creation scripts

---
+++ sql/01_create_tables.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    drink VARCHAR(50),
+    amount DECIMAL(10,2),
+    order_date DATE
+);
